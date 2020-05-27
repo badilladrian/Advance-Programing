42a612a6958b5e0cb875f13b4fd652208aaebb38
badilladrian <badilladrianch@gmail.com>
2020-05-27 10:38:28 -0600

    Update!

---
+++ Django Pollster/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\badil\desktop\all\progra avanzada\django pollster\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- Python Web/3_json/json_request_readdata.py
+++ Python Web/3_json/json_request_readdata.py
@@ -8,32 +8,37 @@ import json
 def main():
     # Use requests to issue a standard HTTP GET request
     url = "http://httpbin.org/json"
-    result = requests.get(url)
 
-    # TODO: Use the built-in JSON function to return parsed data
-    data_object = result.json()
+    response = requests.get(url)
+
+    #built-in JSON function to return parsed data
+    json_object = response.json()
+
     #imprime el objeto de json
-    print(data_object)
+    print(json_object.keys())
+    
     #sin embargo si deseo imprimirlo lo correcto es transformalo
     #a json_string con dumps()
-    print(json.dumps(data_object,indent=2))
-    # TODO: Access data in the python object
+    json_en_python = json.dumps(json_object,indent=2)
+    print(json_en_python)
+    
+    
     #to know all the keys of the json object
-    print(list(data_object.keys()))
+    print(list(json_en_python.keys()))
 
     sub_keys_list = []
-    for sub_key in data_object['slideshow']:
+    for sub_key in json_en_python['slideshow']:
         print(sub_key)
         sub_keys_list.append(sub_key)
     
     slides = []
-    for titulos in data_object['slideshow'][sub_keys_list[2]]:
+    for titulos in json_en_python['slideshow'][sub_keys_list[2]]:
         slides.append(titulos)
     
     slide1 = slides[0]
     slide2 = slides[1]
 
-    print("Voy a imprimir los {0} de los # {1} {2}:\n{3}\n{4}".format(sub_keys_list[3],len(data_object['slideshow']['slides']),sub_keys_list[2],slide1['title'], slide2['title']))
+    print("Voy a imprimir los {0} de los # {1} {2}:\n{3}\n{4}".format(sub_keys_list[3],len(json_en_python['slideshow']['slides']),sub_keys_list[2],slide1['title'], slide2['title']))
 
 if __name__ == "__main__":
-    main()
+    main()31328-929*
